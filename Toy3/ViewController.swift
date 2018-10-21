5140797f53967e59d04277b61a559183d1283204
Shuyan Li <shuyanli@ma8534-appdev.local>
2018-10-20 19:37:02 -0500

    Initial Commit

---
+++ Toy3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toy3
+//
+//  Created by Shuyan Li on 10/20/18.
+//  Copyright © 2018 Shuyan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Toy3Tests/Toy3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Toy3Tests.swift
+//  Toy3Tests
+//
+//  Created by Shuyan Li on 10/20/18.
+//  Copyright © 2018 Shuyan Li. All rights reserved.
+//
+
+import XCTest
+@testable import Toy3
+
+class Toy3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
